484ce08388db3339bb438538ec4cf2f904eb8dd3
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2024-11-26 19:13:43 -0800

    Update dependencies change to facebook-bot

--- index.js
+++ index.js
@@ -21,35 +21,35 @@ puppeteer.use(
     const page = await browser.newPage();
 
     // Step 4: Navigate the page to a URL <-
-    await page.goto("https://www.airbnb.com/login", {
+    await page.goto("https://www.facebook.com/", {
         waitUntil: "networkidle0"
     });
 
-    // Click email button
-    await page.waitForSelector('[data-testid="social-auth-button-email"]', {
-        timeout: 5000
-    });
-    await page.click('[data-testid="social-auth-button-email"]');
+    // // Click email button
+    // await page.waitForSelector('[data-testid="social-auth-button-email"]', {
+    //     timeout: 5000
+    // });
+    // await page.click('[data-testid="social-auth-button-email"]');
 
-    // Fill out email
-    setTimeout(async () => {
-        await page
-            .locator("._1gj8ihx")
-            .fill(process.env.email);
-        await page
-            .locator('[data-testid="signup-login-submit-btn"]')
-            .click();
-    }, Math.floor(Math.random() * 10000));
+    // // Fill out email
+    // setTimeout(async () => {
+    //     await page
+    //         .locator("._1gj8ihx")
+    //         .fill(process.env.email);
+    //     await page
+    //         .locator('[data-testid="signup-login-submit-btn"]')
+    //         .click();
+    // }, Math.floor(Math.random() * 10000));
 
-    // fill out password
-    await page
-        .locator('[data-testid="email-signup-password"]')
-        .fill(process.env.password);
-    await page
-        .locator('[data-testid="signup-login-submit-btn"]')
-        .click();
+    // // fill out password
+    // await page
+    //     .locator('[data-testid="email-signup-password"]')
+    //     .fill(process.env.password);
+    // await page
+    //     .locator('[data-testid="signup-login-submit-btn"]')
+    //     .click();
 
-    // Step 5: Take a screenshot and save it <-
+    // // Step 5: Take a screenshot and save it <-
     await page.screenshot({ path: "example.png" });
 
     // Step 6: Close the browser <-
